474d73b997d8dce3fe387e19491771b3fd8dd240
SunnyBro1056 <sun+nxisoftgithub@coinblock25.com>
2026-05-10 22:50:15 -0700

    Add chapter functionality to question searching

--- cogs/questions.py
+++ cogs/questions.py
@@ -45,7 +45,7 @@ class Prompter(commands.Cog):
 # GET RANDOM QUESTION #
 #######################
 
-    async def get_question(self, subject: str) -> tuple[tuple[str, Question] | None, str | None]:
+    async def get_question(self, subject: str, chapter: str | None) -> tuple[Question | None, str | None]:
         """This function chooses a random question from a given subject
         and returns a Question object with shuffled answers. If any problems
         arise, the function passes an error message string."""
@@ -54,20 +54,28 @@ class Prompter(commands.Cog):
         # Ensure user entered a valid subject
         if subject not in self.tests:
             return None, "Subject not found."
-
-        # Get a random chapter and random question from the given subject
         current_test = self.tests[subject]
+       
+        # If user did not specify a chapter, use a random one
+        if chapter is None:
+            # This collects only chapters with questions in them
+            valid_chapters = [
+                chapter for chapter, questions in current_test.items()
+                if questions
+            ]
+            if not valid_chapters:
+                return None, "No chapters with questions."
+            # Choose a chapter, then choose a random question from that chapter
+            chapter = random.choice(valid_chapters)
 
-        # This collects only chapters with questions in them
-        valid_chapters = [
-            chapter for chapter, questions in current_test.items()
-            if questions
-        ]
-        if not valid_chapters:
-            return None, "No chapters with questions."
+        # If user entered a chapter, validate it.
+        else:
+            if chapter not in current_test:
+                return None, "Chapter not found."
+            if not current_test[chapter]:
+                return None, "Chapter has no valid questions."
 
-        # Choose a chapter, then choose a random question from that chapter
-        chapter = random.choice(valid_chapters)
+        # Pick a random question from the subject - chapter and ensure it exists in the questions file
         question = random.choice(current_test[chapter])
         if question not in self.questions:
             return None, "Question UUID missing"
@@ -86,23 +94,23 @@ class Prompter(commands.Cog):
             str(current['explanation'])
         )
 
-        return (chapter, question_obj), None
+        return question_obj, None
 
 ################
 # EXAM COMMAND #
 ################
 
     @app_commands.command(name="exam", description="Receive a fixed number of questions")
-    @app_commands.describe(subject="Choose a subject", number="The amount of questions")
+    @app_commands.describe(subject="Choose a subject", chapter="Optional chapter filter", number="The amount of questions")
     async def exam(
         self,
         interaction: discord.Interaction,
         subject: str,
-        number: int
+        number: int,
+        chapter: str | None = None
     ):
         """This function holds the '/exam' command. It takes a subject and a number of questions for the
         user, and continues to ask the user questions after they answer the previous question."""
-        chapter: str
         question_obj: Question
         msg: Any # Typecasting got too hard so I gave up
 
@@ -110,22 +118,26 @@ class Prompter(commands.Cog):
         if not 0 < number < 30:
             await interaction.response.send_message("Only a maximum of 30 questions in a row are supported.")
             return
+        
+        # TODO: separate logic in get_questions function so we can validate if a subject has chapters
+        # or if a chapter has questions. The way this is right now, it sends two messages before it errors
+        # out and is kind of janky.
 
         # Loops so that user can answer questions without typing the command every time
         await interaction.response.send_message(f"Starting an exam with {number} questions..")
         for _ in range(number):
 
             # Get a random question with error handling
-            result, error = await self.get_question(subject)
+            result, error = await self.get_question(subject, chapter)
             if error or result is None:
                 await interaction.followup.send(error or "Unknown error")
                 return
-            chapter, question_obj = result
+            question_obj = result
 
             # Build the initial embed for the question
             embed = discord.Embed(
                 color = discord.Color.blue(),
-                title = f"Question: {subject}, {chapter}",
+                title = f"Question: {subject} {chapter if chapter else ""}",
                 description = (
                     f"{question_obj.prompt}\n\n"
                     + "\n".join(
@@ -154,7 +166,7 @@ class Prompter(commands.Cog):
             # Build the new embed
             new_embed = discord.Embed(
                 color = embed_color,
-                title = f"Question: {subject}, {chapter}",
+                title = f"Question: {subject} {chapter if chapter else ""}",
                 description = (
                     f"{question_obj.prompt}\n\n"
                     + "\n".join(
@@ -172,12 +184,12 @@ class Prompter(commands.Cog):
 ####################
 
     @app_commands.command(name="question", description="A test question command")
-    @app_commands.describe(subject="Choose a subject")
+    @app_commands.describe(subject="Choose a subject", chapter="Optional chapter filter")
     async def question(
         self, 
         interaction: discord.Interaction, 
         subject: str, 
-        #chapter: float | None = None TODO
+        chapter: str | None = None
     ):
         """This is the main 'question' command which searches a test
         in the self.tests memory for a specific test. Each test holds
@@ -185,16 +197,16 @@ class Prompter(commands.Cog):
         questions."""
 
         # Pick a random question with error handling
-        result, error = await self.get_question(subject)       
+        result, error = await self.get_question(subject, chapter)       
         if error or result is None:
             await interaction.response.send_message(error)
             return
-        chapter, question_obj = result
+        question_obj = result
 
         # Build the initial embed for the question
         embed = discord.Embed(
             color = discord.Color.blue(),
-            title = f"Question: {subject}, {chapter}",
+            title = f"Question: {subject} {chapter if chapter else ""}",
             description = (
                 f"{question_obj.prompt}\n\n"
                 + "\n".join(
@@ -222,7 +234,7 @@ class Prompter(commands.Cog):
         # Build the new embed
         new_embed = discord.Embed(
             color = embed_color,
-            title = f"Question: {subject}, {chapter}",
+            title = f"Question: {subject} {chapter if chapter else ""}",
             description = (
                 f"{question_obj.prompt}\n\n"
                 + "\n".join(
@@ -248,6 +260,26 @@ class Prompter(commands.Cog):
         return [
             app_commands.Choice(name=subject, value=subject)
             for subject in self.tests if current.lower() in subject.lower()
+        ]
+
+    @question.autocomplete('chapter')
+    @exam.autocomplete('chapter')
+    async def chapter_autocomplete(
+        self,
+        interaction: discord.Interaction,
+        current: str
+    ) -> list[app_commands.Choice[str]]:
+
+        subject = getattr(interaction.namespace, "subject", None)
+
+        if not subject or subject not in self.tests:
+            return []
+
+        chapters = self.tests[subject].keys()
+
+        return [
+            app_commands.Choice(name=chapter, value=chapter)
+            for chapter in chapters if current.lower() in chapter.lower()
         ]
 
 ##################
